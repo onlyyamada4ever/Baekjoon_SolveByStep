feed6ddc68b928c0816f462d547e30aa277f7c25
Elif_918 <onlyyamada4ever@gmail.com>
2026-02-16 10:21:02 +0400

    added 2588.py
    Implement multiplication logic for two input numbers.

---
+++ Py3/입출력과 사칙연산/2588.py
@@ -0,0 +1,22 @@
+using System;
+namespace Baekjoon
+{
+    class Baekjoon2588
+    {
+        static void Main(string []args) 
+        {
+            string A = Console.ReadLine(); // 입력값을 "A"에 저장
+            string B = Console.ReadLine(); // 입력값을 "B"에 저장
+            char[] num = B.ToCharArray(); // "B" 를 문자 하나하나로 나눠 "num"에 저장
+            int X = int.Parse(A); // "A"를 정수형으로 변환해 "X"에 저장
+            int Y = int.Parse(B); // "B"를 정수형으로 변환해 "Y"에 저장
+            
+            Console.WriteLine(X * int.Parse(num[2].ToString())); // 1의 자리 계산
+            Console.WriteLine(X * int.Parse(num[1].ToString())); // 10의 자리 계산
+            Console.WriteLine(X * int.Parse(num[0].ToString())); // 100의 자리 계산
+            Console.WriteLine(X * Y); // 전체 계산 결과 출력
+        }
+    }
+}
+// 백준 단계별로 풀어보기 1. 입출력과 사칙연산 - 10단계 2588번 곱셈
+// https://www.acmicpc.net/problem/2588
