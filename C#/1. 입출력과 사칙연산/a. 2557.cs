431383c444e62163d5b56ebb851f4f1f879cedf2
Elif_918 <onlyyamada4ever@gmail.com>
2026-02-24 10:30:52 +0400

    2557

---
+++ C#/1. 입출력과 사칙연산/a. 2557.cs
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/2557
+// 백준 단계별로 풀어보기 - 입출력과 사칙연산
+// 1단계. 2557 Hello World
+
+// 시간 제한 : 1초
+// 메모리 제한 : 128 MB
+// 문제 : Hello World! 를 출력하시오.
+// 입력 : 없음
+// 출력 : Hello World! 를 출력하시오
+
+using System;
+namespace Baekjoon{
+    // Baekjoon namespace의
+    class BaekjoonSBS{
+        // BaekjoonSBS class에서
+        static void Main(string[] args){
+            // Main함수를 실행
+            Console.WriteLine("Hello World!");
+            // Console에 Hello World! 출력
+        }
+    }
+}
