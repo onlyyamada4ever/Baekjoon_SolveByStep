ee010b2bba6500838fc56f7d69e0b1f78cd87683
Elif_918 <onlyyamada4ever@gmail.com>
2026-04-02 13:25:11 +0400

    Update e. 1008.cpp

--- C++/1. 입출력과 사칙연산/e. 1008.cpp
+++ C++/1. 입출력과 사칙연산/e. 1008.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+
+int main() {
+    double A; double B;
+    std::cin >> A;
+    std::cin >> B;
+    
+    std::cout.precision(16); // 출력 자릿수 고정(전체)
+    std::cout << std::fixed; // 출력 자릿수 고정(~소수점)
+    std::cout << A/B;
+    return 0;
+}
+/*
 #include <stdio.h>
 
 int main() {
@@ -8,3 +21,4 @@ int main() {
     // 또한 9자리 이상으로 출력해야 하기 때문에 .{n}으로 출력할 자릿수도 지정해줌
     return 0;
 }
+*/
