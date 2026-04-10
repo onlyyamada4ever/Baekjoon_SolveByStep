1b76b71c433de8b1630f0cca2f811951dae7a633
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-04-10 14:46:02 +0400

    nn

---
+++ C#/5. 반복문/a. 27866.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Baekjoon
+{
+    class Baekjoon27866
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int cord = int.Parse(Console.ReadLine());
+            Console.WriteLine(input[cord-1]);
+        }
+    }
+}
