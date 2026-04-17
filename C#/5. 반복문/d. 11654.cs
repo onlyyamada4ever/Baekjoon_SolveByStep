6fbdd3770df5e41645f07ccc07c361de82681cc9
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-04-17 11:38:35 +0400

    Create d. 11654.cs

---
+++ C#/5. 반복문/d. 11654.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Baekjoon
+{
+    class Baekjoon11654
+    {
+        static void Main(string[] args)
+        {
+            char ascii = char.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToInt32(ascii));
+        }
+    }
+}
