b67b8fc9f40138edbd0dd07d320ed28453bb2454
Elif_918 ⋆⁺₊ <onlyyamada4ever@gmail.com>
2026-04-17 10:31:34 +0400

    2743

---
+++ C#/5. 반복문/b. 2743.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Baekjoon {
+    class Baekjoon2743 {
+        static void Main(string[] args) {
+           string input = Console.ReadLine();
+           Console.WriteLine(input.Length);
+        }
+    }
+}
